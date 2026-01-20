adada06cab15c69288e8c70d2c7b33d1c3aa7b69
freelanceradnan <reactorbro722@gmail.com>
2026-01-20 22:30:52 +0600

    updated added

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // default, Netlify deploy করবে dist folder
-  },
+  base: "/",   // important for Netlify
 });
 
